d774fa56dcde63a40392a2865d488a0646f37b2e
abraham241 <huguesmboundou@gmail.com>
2025-03-07 13:17:55 +0100

    ajout total des card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,28 @@ import Image from "next/image";
 import NavBar from "@/components/navBar";
 import Province from "@/components/province";
 import DestinationCard from "@/components/card-home/destinationCard";
+import DestinationCardTwo from "@/components/card-home/destinationCardTwo";
+import DestinationCardThree from "@/components/card-home/destinationCardThree";
+import DestinationCardFor from "@/components/card-home/destinationCardFor";
 
 export default function Home() {
   return (
     <div className="md:px-12 md:py-2 ">
       <NavBar />
       <Province />
-      <div>
-        <DestinationCard />
+      <div className="flex justify-center items-center gap-4 p-4 flex-wrap md:flex-nowrap">
+        <div className="w-full md:w-1/4">
+          <DestinationCard />
+        </div>
+        <div className="w-full md:w-1/4">
+          <DestinationCardTwo />
+        </div>
+        <div className="w-full md:w-1/4">
+          <DestinationCardThree />
+        </div>
+        <div className="w-full md:w-1/4">
+          <DestinationCardFor />
+        </div>
       </div>
     </div>
   );
